aab5d9a3c572d6b8e945d27db9689ce133128fa4
dida0982 <dida0982@gmail.com>
2026-02-20 18:12:58 +0000

    20 de fev. de 2026, 15:12

--- src/python/ex8.py
+++ src/python/ex8.py
@@ -1,4 +1,6 @@
 valor = float(input("Qual o valor do produto?"))
 taxa = float(input("Quanto é a taxa?"))
+tempo = float(input("Qual o valor do produto?"))
+
 
 prestacao = valor + (valor*(taxa/100)*tempo)
