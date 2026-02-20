ea6cc36170e23ffa284df18ced4cfb618b26c5cf
dida0982 <dida0982@gmail.com>
2026-02-20 14:35:19 +0000

    20 de fev. de 2026, 11:35

--- src/python/ex4.py.py
+++ src/python/ex4.py.py
@@ -1,2 +1,5 @@
-grausC = float(input("Digite uma temperatura em graus Celsius, que o código vai converter para graus Fahrenheit: "))
+c = float(input("Digite uma temperatura em graus Celsius, que o código vai converter para graus Fahrenheit: "))
 
+f = c*9/5+32
+
+print("Celsius: ", c, "Fahrenhait: ", f)
