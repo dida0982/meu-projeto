1f346ab3f60001a0f72cc364ae6e767455f50e15
dida0982 <dida0982@gmail.com>
2026-02-20 12:22:21 +0000

    20 de fev. de 2026, 09:22

--- src/python/ex1.py
+++ src/python/ex1.py
@@ -1,4 +1,5 @@
+float(input("Salário Bruto: "))
 float(input("Salário Líquido: "))
 float(input("Valor da hora: "))
-float(input("Percentual do desconto do INSS"))
 float(input("Número de horas trbalhadas no mes: "))
+float(input("Percentual do desconto do INSS"))
