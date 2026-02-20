35814d58eb5c608b278c237e75cbce77b1d26f71
dida0982 <dida0982@gmail.com>
2026-02-20 16:49:28 +0000

    20 de fev. de 2026, 13:49

--- src/python/ex7.py
+++ src/python/ex7.py
@@ -1,4 +1,7 @@
 #12km por litro
 #quanto de combustível gasta em uma viagem ?
 #tempo gasto
-#
+velocidade = float(input("Qual a velocidade do carro?"))
+tempo = float(input("Quanto tempo duro a viagem?"))
+
+distancia = tempo * vel
