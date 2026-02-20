16cda2650463e5da1863373e470f5bac33be8ba7
dida0982 <dida0982@gmail.com>
2026-02-20 17:58:04 +0000

    20 de fev. de 2026, 14:58

--- src/python/ex7.py
+++ src/python/ex7.py
@@ -6,7 +6,7 @@ tempo = float(input("Quanto tempo durou a viagem?"))
 distancia = tempo * velocidade
 litros_usados = distancia/10
 
-print(f"valocidade média: {velocidade:.2f} ")
+print(f"velocidade média: {velocidade:.2f} ")
 print(f"tempo gasto na viagem(em horas): {tempo:.2f}")
 print(f"distancia percorrida: {distancia:.2f}")
 print(f"quantidade de litros utilizada na viagem: {litros_usados:.2f}")
