ce9d9bead8fc9a73c134db9f5ea4dcd6c0e0921e
dida0982 <dida0982@gmail.com>
2026-02-20 18:46:51 +0000

    20 de fev. de 2026, 15:46

--- src/python/ex10.py
+++ src/python/ex10.py
@@ -3,20 +3,23 @@ b = int(input("Valor 2:"))
 c = int(input("Valor 3:"))
 d = int(input("Valor 4:"))
 
-a+b
-a+c
-a+d
+a1 = a + b
+a2 = a + c
+a3 = a + d
 
-b+c
-b+d
+a4 = b + c
+a5 = b + d
 
-c+d
+a6 = c + d
 
-a*b
-a*c
-a*d
+m1 = a * b
+m2 = a * c
+m3 = a * d
 
-b*c
-b*d
+m4 = b * c
+m5 = b * d
 
-c*d
+m6 = c * d
+
+print(a1, a2, a3, a4, a5, a6)
+print(m1, m2, m3, m4, m5, m6)
