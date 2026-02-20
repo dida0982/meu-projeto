0b043160cc5ed828624ca5008b353629e15034f1
dida0982 <dida0982@gmail.com>
2026-02-20 16:05:58 +0000

    20 de fev. de 2026, 13:05

---
+++ src/python/ex5.py
@@ -0,0 +1,5 @@
+c = float(input("Digite uma temperatura em graus Fahrenheit, que o código vai converter para graus Celsius: "))
+
+f = c*9/5+32
+
+print("Graus Fahrenheit:", f," | ", "Graus Celsius: ", c)
