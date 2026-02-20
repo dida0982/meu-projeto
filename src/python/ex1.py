d45b0161a8fa6dd1d817bbf25b5f8b1fe87adebc
dida0982 <dida0982@gmail.com>
2026-02-20 13:35:02 +0000

    20 de fev. de 2026, 10:35

--- src/python/ex1.py
+++ src/python/ex1.py
@@ -1,12 +1,12 @@
-# SB = float(input("Salário Bruto: "))
-# SL = float(input("Salário Líquido: "))
+SB = float(input("Salário Bruto: "))
+SL = float(input("Salário Líquido: "))
 VH = float(input("Valor da hora-aula: "))
 HT = int(input("Número de horas trbalhadas no mes: "))
 PD = float(input("Percentual do desconto do INSS"))
-# TD = float(input("Total de desconto"))
+TD = float(input("Total de desconto"))
 
 SB = HT * VH
-PD/100 = TD
-SL = SL - SB
+TD = (PD/100) * SB
+SL = SB - TD
 
 print("Salário bruto: , ", SB, "Salário Líquido: ", SL)
