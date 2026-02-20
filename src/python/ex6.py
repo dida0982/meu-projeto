9832c802b0e382dfbb4d718c163271c354fd9857
dida0982 <dida0982@gmail.com>
2026-02-20 16:19:48 +0000

    20 de fev. de 2026, 13:19

--- src/python/ex6.py
+++ src/python/ex6.py
@@ -1,3 +1,6 @@
 print("O código vai calculator o volume de uma lata de óleo para voce")
 altura = float(input("Qual a altura da lata de óleo? "))
-volume = 3.14159*r**2*altura
+raio = float(input("Qual é o ráio da lata de óleo? "))
+volume = 3.14159*raio**2*altura
+
+print("O volume da lata de óleo é: ", volume,)
