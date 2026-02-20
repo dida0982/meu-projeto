dca32e529aa0710a9fa6609a60fdad23ce029657
dida0982 <dida0982@gmail.com>
2026-02-20 12:30:14 +0000

    20 de fev. de 2026, 09:30

--- src/python/ex1.py
+++ src/python/ex1.py
@@ -3,5 +3,8 @@ SL = float(input("Salário Líquido: "))
 VH = float(input("Valor da hora-aula: "))
 HT = float(input("Número de horas trbalhadas no mes: "))
 PD = float(input("Percentual do desconto do INSS"))
+TD = float(input("Total de desconto"))
 
 HT * VH = SB
+PD/100 = TD
+
