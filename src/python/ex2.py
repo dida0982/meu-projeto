b58f7b7b6b7f5529fa8608d3c7e2635dc20d7ddf
dida0982 <dida0982@gmail.com>
2026-02-20 13:42:18 +0000

    20 de fev. de 2026, 10:42

---
+++ src/python/ex2.py
@@ -0,0 +1,6 @@
+#desenvolva um program que que faça a entrada do nome de uma pessoa e de seu sexo, apresentando em seguida os dados enteriores informados.
+
+input("Seu nome:")
+input("Seu sexo:")
+
+
