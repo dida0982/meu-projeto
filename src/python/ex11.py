208eab2bcfb254a7ef4d1baa4664142a2407630f
dida0982 <dida0982@gmail.com>
2026-02-21 02:08:35 +0000

    20 de fev. de 2026, 23:08

--- src/python/ex11.py
+++ src/python/ex11.py
@@ -1,8 +1,8 @@
 print("Este código calcula o volume de uma caixa")
 
-comprimento = float(input("Qual o coprimento da caixa? "))
-largura = float(input("Qual a largura da caixa?"))
-altura = float(input("Qual a altura da caixa?"))
+comprimento = float(input("Qual o coprimento da caixa ? "))
+largura = float(input("Qual a largura da caixa? "))
+altura = float(input("Qual a altura da caixa? "))
 
 
 volume = comprimento * largura * altura
