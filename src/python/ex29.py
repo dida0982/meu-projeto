4c2285c1792b4b9b2fb8f39d3beacc51012cbf46
dida0982 <dida0982@gmail.com>
2026-02-23 23:45:49 +0000

    23 de fev. de 2026, 20:45

--- src/python/ex29.py
+++ src/python/ex29.py
@@ -4,7 +4,7 @@ import string
 
 
 name = string(input("What is your name?"))
-old = int(input("How old are you?"))
+age = int(input("How age are you?"))
 height= float(input("How height are you?"))
 
 print(f"Your name is {name}, you are {age} years old and {height} tall")
