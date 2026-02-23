15b7e8b3ba8da337e2f9116c27554bd817dbc173
dida0982 <dida0982@gmail.com>
2026-02-23 14:57:16 +0000

    23 de fev. de 2026, 11:57

--- src/python/ex20.py
+++ src/python/ex20.py
@@ -1,5 +1,9 @@
-sm = int(input("Salário mensal: "))
-pr = int(input("pecentual de reajuste: "))
+print(" ")
+print("Este código vai fazer o cálculo de um reajuste salarial.")
+print(" ")
+
+sm = int(input("Qual o salário mensal: "))
+pr = int(input("Qual o percentual do reajuste? (Ex: se for 10 porcento coloca 1.10, se for 20 porcento coloca 1.20, se for de 5 porcento coloca 1.05 etc...): "))
 
 ns = sm*(1+0.10)
 
