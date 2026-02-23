04045c0e34a42694694edfa067f20ddda666d121
dida0982 <dida0982@gmail.com>
2026-02-23 16:01:17 +0000

    23 de fev. de 2026, 13:01

--- src/python/ex22.py
+++ src/python/ex22.py
@@ -1,1 +1,4 @@
-print("Este código vai fazer o cálculo da velocidade de um projétil que percorre uma distância em km em um tempo de segundos. Vai ser apresentado em metros por segundo. ")
+print("Este código vai fazer o cálculo da velocidade de um projétil que percorre uma distância em km em um espaço de tempo em minutos. Vai ser apresentado em metros por segundo. ")
+
+
+velocidade = (distancia * 1000 / tempo * 60)
