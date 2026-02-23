bd418ce176c5dffa1e127b4c824e06c1e69fcf71
dida0982 <dida0982@gmail.com>
2026-02-23 15:50:28 +0000

    23 de fev. de 2026, 12:50

--- src/python/ex21.py
+++ src/python/ex21.py
@@ -1,4 +1,11 @@
+print("Este código vai apresentar o cálculo das quatro operações aritiméticas básicas")
+
 a = int(input("Digite um valor inteiro para a:"))
 b = int(input("Digite o valor inteiro para b:"))
 
+ad = a+b
+sb = a-b
+mt=a*b
+dv=a/b
 
+print(f"Adição de a por b: {ad} | Subtração de a por b: {sb} | Multiplicação de a por b: {mt} | Divisão de a por b: {dv} ")
