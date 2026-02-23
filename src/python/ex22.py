3837b9dc993b62aeb476275eca24494698a9796e
dida0982 <dida0982@gmail.com>
2026-02-23 16:09:28 +0000

    23 de fev. de 2026, 13:09

--- src/python/ex22.py
+++ src/python/ex22.py
@@ -4,3 +4,5 @@ distancia = float(input("Quantos metros o projétil percorreu? "))
 tempo = float(input("Quantos segundos ele demorou?"))
 
 velocidade = (distancia * 1000 / tempo * 60)
+
+print(f"O projétil se deslocou a uma velocidade de {distancia}/{tempo}")
