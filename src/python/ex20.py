b16950a197a0a076266360833c935d39548666d8
dida0982 <dida0982@gmail.com>
2026-02-23 14:45:33 +0000

    23 de fev. de 2026, 11:45

--- src/python/ex20.py
+++ src/python/ex20.py
@@ -1,6 +1,6 @@
 sm = int(input("Salário mensal: "))
 pr = int(input("pecentual de reajuste: "))
 
-ns = 3000+10/100
+ns = 3000*(1+0.10)
 
-print(f"O novo salário é de: {ns:.2f}")
+print(f"O novo salário com o 10% de ajuste: {ns:.2f}")
