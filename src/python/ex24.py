42b41cf9d5090f7ca5ea8dfc038a7cbb883a4007
dida0982 <dida0982@gmail.com>
2026-02-23 20:15:28 +0000

    23 de fev. de 2026, 17:15

--- src/python/ex24.py
+++ src/python/ex24.py
@@ -1,3 +1,9 @@
-a =int(input("Digite o primeiro valor: "))
-b =int(input("Digite o segundo valor: "))
+print("Este código vai fazer o calculo da case para o expoente.")
+print("")
 
+a =int(input("Digite a base: "))
+b =int(input("Digite o expoente: "))
+
+r= a**b
+
+print(f"O resultado é: {r}")
