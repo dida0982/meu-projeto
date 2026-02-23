c2cf69fab3618bf139e524bf3d38325e58f49316
dida0982 <dida0982@gmail.com>
2026-02-23 23:23:56 +0000

    23 de fev. de 2026, 20:23

--- src/python/ex28.py
+++ src/python/ex28.py
@@ -1,4 +1,6 @@
 a = int(input("Enter numeric value a: "))
 b = int(input("Enter numeric value b: "))
 
-r= a**2/b
+r= (a/b)**2
+
+print(f"O quadrado da divisao do valor {a} em rolação ao valor {b} da {r}")
