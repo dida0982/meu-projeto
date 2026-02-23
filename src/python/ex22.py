03d40ddbfc09f0d14976223073fe7ec5b6d981ee
dida0982 <dida0982@gmail.com>
2026-02-23 16:15:09 +0000

    23 de fev. de 2026, 13:15

--- src/python/ex22.py
+++ src/python/ex22.py
@@ -3,4 +3,4 @@ print("Este código vai fazer o cálculo da velocidade de um projétil. ")
 distancia = float(input("Quantos metros o projétil percorreu? "))
 tempo = float(input("Quantos segundos ele demorou?"))
 
-print(f"O projétil se deslocou a uma velocidade de {distancia:.0f}m/{tempo:.0f}s")
+print(f"O projétil se deslocou a uma velocidade de: {distancia:.0f}m/{tempo:.0f}s")
