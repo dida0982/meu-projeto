e2c074116529874802cd56c5363e3974c618ee92
dida0982 <dida0982@gmail.com>
2026-02-23 20:01:45 +0000

    23 de fev. de 2026, 17:01

--- src/python/ex23.py
+++ src/python/ex23.py
@@ -1,8 +1,8 @@
 print("This code will calculate the volume of a sphere.")
 print("")
 
-float(input("What is the radius of a sphere?"))
+r = float(input("What is the radius of the sphere?"))
 
 v= (4/3)*314159*(r**3)
 
-print(f"The volume of the sphere is: ")
+print(f"The volume of the sphere is: {v:.2f}")
