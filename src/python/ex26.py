02d78f388d780e4d3e6bad1b6428a56a65d7eac4
dida0982 <dida0982@gmail.com>
2026-02-23 20:46:29 +0000

    23 de fev. de 2026, 17:46

--- src/python/ex26.py
+++ src/python/ex26.py
@@ -1,1 +1,7 @@
-print("Esse código calculator a raiz ")
+print("Esse código vai calculator a raiz dos números")
+print("")
+
+v = float(input("Digite um valor: "))
+r = float(input("Digite a raiz desse valor: "))
+
+
