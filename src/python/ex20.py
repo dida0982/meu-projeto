8c2f1a2d890e651fee318bd6d1bdd3247a90ce60
dida0982 <dida0982@gmail.com>
2026-02-23 15:14:54 +0000

    23 de fev. de 2026, 12:14

---
+++ src/python/ex20.py
@@ -0,0 +1,8 @@
+print("Este programa vai calcular a área de uma circunferencia.")
+print(" ")
+
+r = float(input("Qual o raio da circunferencia? "))
+
+a = 3.14159265*r**2
+
+print(f"A área da circunferencia é: {a:.2f}")
