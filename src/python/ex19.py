e6a92c9e58e3826a77d6ff4a5d6d6ab8692944ef
dida0982 <dida0982@gmail.com>
2026-02-23 14:24:47 +0000

    23 de fev. de 2026, 11:24

--- src/python/ex19.py
+++ src/python/ex19.py
@@ -3,6 +3,6 @@ b = int(input("Digite o valor de b: "))
 c = int(input("Digite o valor de c: "))
 d = int(input("Digite o valor de d: "))
 
-r = (a+b+c)**2
+p = a+c
 
 print(f"O resultado do quadrado da soma dos tres valores é: {r:.2f}")
