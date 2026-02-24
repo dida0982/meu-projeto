5294bfd873287803ce3e1a1422230d1efd73f799
dida0982 <dida0982@gmail.com>
2026-02-24 00:16:18 +0000

    23 de fev. de 2026, 21:16

---
+++ src/python/ex30 copy.py
@@ -0,0 +1,10 @@
+#Lista de tres pessoas com idade e altura de cada uma.
+
+nomes = []
+idade = []
+altura = []
+
+for i in ranger(3):
+    nome = input(f"Digite um nome: ")
+    idade = input(f"Digite a idade: ")
+    altura = input(f"Digite a altura: ")
