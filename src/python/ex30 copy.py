5879d107939a3016fa91717f594a5d78656e5819
dida0982 <dida0982@gmail.com>
2026-02-24 00:25:40 +0000

    23 de fev. de 2026, 21:25

--- src/python/ex30 copy.py
+++ src/python/ex30 copy.py
@@ -1,10 +1,19 @@
 #Lista de tres pessoas com idade e altura de cada uma.
 
 nomes = []
-idade = []
-altura = []
+idades = []
+alturas = []
 
-for i in ranger(3):
-    nome = input(f"Digite um nome: ")
-    idade = input(f"Digite a idade: ")
-    altura = input(f"Digite a altura: ")
+for i in range(3):
+    nome = input(f"Digite um nome {i+1}: ")
+    idade = int(input(f"Digite a idade {i+1}: "))
+    altura = float(input(f"Digite a altura {i+1}: "))
+
+nomes.append(nome)
+idades.append(idade)
+alturas.append(altura)
+
+print("")
+
+for i in range(3):
+    print(f"Pessoa {i+1}: Nome = {nomes[i]}, Idade = {idades[i]}, Altura = {alturas[i]}")
